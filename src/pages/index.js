92657addf27c911a53731e4f45d8928ed2319722
dioptre <dioptre@gmail.com>
2019-03-12 02:05:26 +0100

    added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Helmet from "react-helmet";
 
 import Layout from '../components/layout';
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
+import pic01 from '../assets/images/mobile.jpg'
+import pic02 from '../assets/images/empty-pills.jpg'
+import pic03 from '../assets/images/mail.jpg'
+import pic04 from '../assets/images/barcode.jpg'
 
 class Homepage extends React.Component {
     render() {
