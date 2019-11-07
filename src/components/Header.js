ffa8141b1343543860eebbcf7c8e15cc2bbf3f9e
Andrew Grosser <dioptre@gmail.com>
2019-11-06 17:19:35 -0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ class Header extends React.Component {
             <section id="header">
                 <div className="inner">                    
                     <img width="75" src={logo}/>
-                    <h1>At <strong>ValidMeds</strong>, We stop fake <br />
-                    medicine from being sold.</h1>
+                    <h1>At <strong>Valid9</strong>, We stop fake <br />
+                    products from being sold.</h1>
                     <p>Over $250B USD of counterfeit pharmaceuticals<br />
                     are sold to the sick and unaware every year.</p>
                     <ul className="actions">
