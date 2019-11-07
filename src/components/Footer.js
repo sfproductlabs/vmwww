b120b2ea19b255c9da601f79aeecda25e7702dca
Andrew Grosser <dioptre@gmail.com>
2019-11-06 17:20:02 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
                     <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                     <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                     <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li> */}
-                    <li><a href="mailto:andrew@validmeds.com" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
+                    <li><a href="mailto:andrew@valid9.com" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
                 <ul className="copyright">
                     <li>&copy; 2019</li><li>Produced by: <a href="http://sfproductlabs.com">SFPL</a></li>
